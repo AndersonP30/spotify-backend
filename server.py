0fb8c669d561a42fcd49273041ece284c56ec075
AndersonP30 <77370401+AndersonP30@users.noreply.github.com>
2025-09-06 21:04:40 -0700

    Update server.py

--- server.py
+++ server.py
@@ -139,4 +139,4 @@ def track():
 if __name__ == "__main__":
     load_tokens()
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000)
